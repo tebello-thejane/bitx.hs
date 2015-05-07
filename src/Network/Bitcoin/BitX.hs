bb2b722ea10c0a4971029e68138fb9f8df0d5dd0
tebello_thejane <zyxoas+fpcomplete@gmail.com>
2015-05-07 02:09:06 -0700

    Top-level re-exports

--- src/Network/Bitcoin/BitX.hs
+++ src/Network/Bitcoin/BitX.hs
@@ -1,2 +1,8 @@
-module Network.Bitcoin.BitX where
+module Network.Bitcoin.BitX
+    (module BitX)
+    where
+
+import Network.Bitcoin.BitX.Types as BitX
+import Network.Bitcoin.BitX.Private as BitX
+import Network.Bitcoin.BitX.Public as BitX
 
