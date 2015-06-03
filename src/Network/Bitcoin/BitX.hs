80fd0b807523f5ff7b234b99cb8de472011a323d
Tebello Thejane <zyxoas@gmail.com>
2015-06-03 18:59:09 +0200

    Forgot to remove Auth from main module.

--- src/Network/Bitcoin/BitX.hs
+++ src/Network/Bitcoin/BitX.hs
@@ -20,7 +20,7 @@ module Network.Bitcoin.BitX
     module Network.Bitcoin.BitX.Private,
     module Network.Bitcoin.BitX.Public,
     module Network.Bitcoin.BitX.Private.Quote,
-    module Network.Bitcoin.BitX.Private.Auth,
+    --module Network.Bitcoin.BitX.Private.Auth,
     module Network.Bitcoin.BitX.Response,
     BitXAesRecordConvert(..),
     POSTEncodeable(..)
@@ -31,6 +31,6 @@ import Network.Bitcoin.BitX.Types
 import Network.Bitcoin.BitX.Private
 import Network.Bitcoin.BitX.Public
 import Network.Bitcoin.BitX.Private.Quote
-import Network.Bitcoin.BitX.Private.Auth
+--import Network.Bitcoin.BitX.Private.Auth
 import Network.Bitcoin.BitX.Types.Internal
 import Network.Bitcoin.BitX.Response
