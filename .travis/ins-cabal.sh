f993aea450871dbcce14e6bdf14811047bd15332
Tebello Thejane <zyxoas@gmail.com>
2015-06-11 18:22:22 +0200

    Second steps at adding stack to travis build.

--- .travis/ins-cabal.sh
+++ .travis/ins-cabal.sh
@@ -2,5 +2,13 @@
 
 set -ev
 
-cabal install --only-dependencies --enable-tests
+travis_retry sudo add-apt-repository -y ppa:hvr/ghc
+travis_retry sudo apt-get update
+time travis_retry sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER
+export PATH=/home/travis/.cabal/bin:/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
+
+echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+travis_retry cabal update
+
+time cabal install --only-dependencies --enable-tests
 
--- .travis/ins-stack.sh
+++ .travis/ins-stack.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
-cabal install stack
-
+wget -q -O- http://download.fpcomplete.com/ubuntu/fpco.key | sudo apt-key add -
+echo 'deb http://download.fpcomplete.com/ubuntu/precise stable main'|sudo tee /etc/apt/sources.list.d/fpco.list
+sudo apt-get update
+time sudo apt-get install stack -y
 
