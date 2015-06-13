47960eb89b6b39af6f0440023a45135d0d902b81
Tebello Thejane <zyxoas@gmail.com>
2015-06-13 09:24:57 +0200

    Nicer timer message in stack build.

--- .travis/stack-build.sh
+++ .travis/stack-build.sh
@@ -3,7 +3,9 @@
 stack build &
 pidA=$!
 
-while true; do sleep 60; echo -e "\033[0;32m1 minute elapsed.\033[0m"; done &
+minutes=0
+
+while true; do sleep 60; ((minutes++)); echo -e "\033[0;32m$minutes minute(s) elapsed.\033[0m"; done &
 pidB=$!
 
 wait $pidA
