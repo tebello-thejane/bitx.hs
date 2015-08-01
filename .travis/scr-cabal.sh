469db005ab2ac1e2a1fa9e8cfdb56c21f63e8102
Tebello Thejane <zyxoas@gmail.com>
2015-08-01 14:11:46 +0200

    Clean up scr-cabal as changing build type to Simple fixed the Config issue.

--- .travis/scr-cabal.sh
+++ .travis/scr-cabal.sh
@@ -6,10 +6,7 @@ cabal --version
 ghc --version
 
 cabal configure --enable-tests -O0 --disable-library-profiling -v2
-cabal install --only-dependencies --force-reinstalls --enable-tests
-cabal configure --enable-tests -O0 --disable-library-profiling -v2
-cabal clean
-rm ~/.cabal/setup-exe-cache/*
+cabal build
 cabal test
 
 cabal check
