aa67378c5e4b33fa6f626a4ea0d6e089a1e5394d
Tebello Thejane <zyxoas@gmail.com>
2015-08-13 12:09:50 +0200

    Doc fix.

--- src/Network/Bitcoin/BitX/Private.hs
+++ src/Network/Bitcoin/BitX/Private.hs
@@ -13,7 +13,7 @@
 -- =The private BitX API.
 --
 -- Each one of the calls takes at least a 'BitXAuth' containing a previously-created API id and
--- secret (created by 'Network.Bitcoin.BitX.Private.Auth.authGrant', or created by visiting
+-- secret (created by visiting
 -- <https://bitx.co/settings#/api_keys>), and will return a 'BitXAPIResponse'..
 --
 -- It would probably be best to create the 'BitXAuth' record using the constructor 'mkBitXAuth' and
